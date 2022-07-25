e3f45bf5740c439f4beb870448b86dd607695741
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-07-25 17:07:19 +0300

    finish

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,74 @@
 const express = require("express");
-
 const router = express.Router();
+const modelsContacts = require("../../models/createContacts");
+
+const { contactsSchema } = require("../../schema/contactsSchema");
+const { createError } = require("../../helpers");
 
 router.get("/", async (req, res, next) => {
-  res.json({ message: "Home template message" });
+  try {
+    const result = await modelsContacts.listContacts();
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await modelsContacts.getContactById(id);
+
+    if (!result) throw createError(404);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
-  res.json({ message: "template message" });
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) throw createError(400, error.message);
+
+    const result = await modelsContacts.addContact(req.body);
+
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = modelsContacts.removeContact(id);
+
+    if (!result) throw createError(404);
+
+    res.json({
+      message: "Contact deleted",
+    });
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.put("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.put("/:id", async (req, res, next) => {
+  try {
+    const { error } = contactsSchema.validate(req.body);
+    if (error) throw createError(400, error.message);
+
+    const { id } = req.params;
+
+    const result = await modelsContacts.updateById(id, req.body);
+    if (!result) throw createError(404);
+
+    res.json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
