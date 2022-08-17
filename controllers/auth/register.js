63a0189036cd59bbfd941908475182131f1031d7
Vasylij-Kashtalyan <88617002+Vasylij-Kashtalyan@users.noreply.github.com>
2022-08-17 18:25:15 +0300

    finish

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,7 +1,8 @@
 const { User, schemas } = require("../../models/user");
-const { createError } = require("../../helpers");
+const { createError, sendEmail } = require("../../helpers");
 const bcrypt = require("bcryptjs");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
 const register = async (req, res) => {
   const { error } = schemas.register.validate(req.body);
@@ -15,10 +16,19 @@ const register = async (req, res) => {
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email);
 
+  const verificationToken = nanoid();
+  const mail = {
+    to: email,
+    subject: "Підтвердження регістрації",
+    html: `<a target="_blank" href="http://localhost:3000/api/auth/verify/${verificationToken}">Натисніть для підтвердження регістрації </a>`,
+  };
+  await sendEmail(mail);
+
   const result = await User.create({
     ...req.body,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
 
   res.status(201).json({
